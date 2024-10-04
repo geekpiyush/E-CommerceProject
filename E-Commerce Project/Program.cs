70373563d42a11addda1d59cd38a40dd78f9d09e
Mr Black <Mr Black@DESKTOP-B62IE6S>
2024-10-04 11:51:38 -0700

    connection string updated

--- E-Commerce Project/Program.cs
+++ E-Commerce Project/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options=>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddIdentity<ApplicationUser, ApplicationUserRole>(options =>
